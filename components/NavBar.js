150df33ad6fc632fc5b3bf96db1055ea8d5ad583
0xSunil <sunday7637@gmail.com>
2023-01-30 13:00:40 +0530

    added home page hero section

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,16 +6,20 @@ const NavBar = (props) => {
   let con = props.con;
   return (
     <nav className="w-full xl:px-0 sm:px-16 px-6 flex py-6 justify-between items-center navbar text-white">
-     
-        <h1 className="font-1 font-poppins font-semibold ss:text-[52px] text-[32px] text-white ">
-        <Link href="/">BLOG</Link>
-        </h1>
-     
+      <h1 className="font-1 font-poppins font-semibold ss:text-[52px] text-[32px] text-white ">
+        <Link href="/">
+          BL
+          <span className="text-yellow-300">O</span>G
+        </Link>
+      </h1>
 
       {con && (
         <ul className="list-none flex justify-end items-center flex-1">
-          <li className="menu font-poppins font-normal  text-dimWhite cursor-pointer text:[18px] sm:text-[26px]  hover ">
-            All Links
+          <li className="menu font-poppins font-normal  text-dimWhite cursor-pointer text:[18px] sm:text-[26px] mr-5 sm:mr-10  hover ">
+            <Link href="/posts">All Posts</Link>
+          </li>
+          <li className="menu font-poppins font-normal  text-dimWhite cursor-pointer text:[18px] sm:text-[26px] hover ">
+            <Link href="/contect">Contact</Link>
           </li>
         </ul>
       )}
--- components/homePage/Hero.js
+++ components/homePage/Hero.js
@@ -2,24 +2,23 @@ import Image from "next/image";
 import React from "react";
 import profile from "../../public/profile.jpg";
 const Hero = (props) => {
-  
   return (
     <section className="flex justify-center items-center flex-col">
       <div>
         <Image
           src={profile}
           alt="Sunil Reddy"
-          className="rounded-full h-52 w-52"
+          className="rounded-full h-52 w-52 sm:h-72 sm:w-72"
         />
         <div className="absolute z-5 right-1/6 top-0 w-[20%] h-[40%] rounded-full white__gradient"></div>
         <div className="absolute z-0 right-1/6 top-0 w-[20%] h-[40%] rounded-full pink__gradient"></div>
       </div>
       <h1 className="font-1 font-poppins font-semibold sm:text-[52px] text-[35px] text-white ss:leading-[100px] leading-[75px]">
-        HI, I'M SUNIL
+        HI , I'M <span className="text-yellow-300"> SUNIL </span>
       </h1>
       <p className="font-poppins font-normal text-dimWhite text-[14px] sm:text-[18px] leading-[30.8px] text-center">
-        Love to Spend <span className="text-white"> Sleepless </span> Night With{" "}
-        <span className="text-white"> Nextjs </span>
+        Love to Spend <span className="text-yellow-300"> Sleepless </span> Night
+        With <span className="text-yellow-300"> Nextjs </span>
       </p>
     </section>
   );
