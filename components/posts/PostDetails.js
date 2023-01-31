33ae0b43c0d39da57770d24e63d851a308272901
0xSunil <sunday7637@gmail.com>
2023-01-31 22:59:07 +0530

    added markup with styels

--- components/posts/PostDetails.js
+++ components/posts/PostDetails.js
@@ -4,13 +4,13 @@ import classes from "./markdown.module.css";
 const PostDetails = (props) => {
   const { post } = props;
   return (
-    <article className="text-white">
-      <h1 className="font-1 font-poppins font-semibold sm:text-[32px] text-[25px] text-yellow-300">
+    <article className="text-white sm:px-10 lg:px-20 ">
+      <h1 className="font-1 font-poppins font-semibold sm:text-[45px] text-[30px]  white text-center">
         {post.title}
         <div className="absolute right-15 top-10 z-0 right-1/6 w-[20%] h-[20%] rounded-full white__gradient"></div>
         <div className="absolute right-10 top-10 z-0 right-1/6 w-[20%] h-[40%] rounded-full pink__gradient"></div>
       </h1>
-      <div className="">
+      <div>
         <ReactMarkdown className={classes.reactMarkDown}>
           {post.content}
         </ReactMarkdown>
