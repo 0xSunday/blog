c167aa6645b49d3dbe319bf1fb89a7008a495b15
0xSunil <sunday7637@gmail.com>
2023-02-01 09:42:30 +0530

    added data fetching markdown code

--- components/posts/PostDetails.js
+++ components/posts/PostDetails.js
@@ -7,11 +7,11 @@ const PostDetails = (props) => {
   const { post } = props;
 
   const costomRenderers = {
-    Code(code) {
+    code(code) {
       const { className, children } = code;
       const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
       return (
-        <SyntaxHighlighter language="javascript" style={dark}>
+        <SyntaxHighlighter language="javascript" style={atomDark}>
           {codeString}
         </SyntaxHighlighter>
       );
@@ -19,7 +19,7 @@ const PostDetails = (props) => {
   };
   return (
     <article className="text-white sm:px-10 lg:px-20 h">
-      <h1 className="font-1 font-poppins font-semibold sm:text-[45px] text-[30px]  white text-center">
+      <h1 className="font-1 font-poppins font-semibold -mb-5 sm:text-[45px] text-[30px]  white text-center">
         {post.title}
         <div className="absolute right-15 top-10 z-0 right-1/6 w-[20%] h-[20%] rounded-full white__gradient"></div>
         <div className="absolute right-10 top-10 z-0 right-1/6 w-[20%] h-[40%] rounded-full pink__gradient"></div>
